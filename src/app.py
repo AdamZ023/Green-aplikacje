bd2f6d750e598b9d3e3ba5d27c2797fb2581b4ce
AdamZ023 <adamzareba@vp.pl>
2025-06-04 08:12:41 +0200

    Dodaję plik src/app.py z przykładową funkcją hello()

---
+++ src/app.py
@@ -0,0 +1,8 @@
+ef hello(name: str) -> str:
+    """
+    Prosta funkcja zwracająca powitanie po imieniu.
+    """
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(hello("Codex"))
